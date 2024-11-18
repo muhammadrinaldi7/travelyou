76ba1ceb635a80fdc5053f2e1f3cfd9108b6ee44
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2024-11-18 10:08:40 +0800

    Fix Variabel Never User

--- src/components/section/ActivitySection.tsx
+++ src/components/section/ActivitySection.tsx
@@ -1,85 +1,90 @@
-import React from 'react';
-import Image from 'next/image';
-import { faCompass,  faStar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Link from 'next/link';
-import { useFetchActivity } from '@/api/hooks/Activity/useFetchActivity';
-import endpoints from '@/api/endpoints';
-
-
-
+import React from "react";
+import Image from "next/image";
+import { faCompass, faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { useFetchActivity } from "@/api/hooks/Activity/useFetchActivity";
+import endpoints from "@/api/endpoints";
 
 const ActivitySection: React.FC = () => {
-    const {data:activityCategories,isLoading} = useFetchActivity(endpoints.activity)
-    return (
-        <section id='activity' className="container px-4 py-16 mx-auto bg-gradient-to-b from-white to-primary-300">
-            {/* Judul Section */}
-            <div className="mb-12 text-center">
-                <h2 className="mb-4 text-4xl font-bold text-gray-800">
-                    Explore Amazing Activities
-                </h2>
-                <p className="max-w-2xl mx-auto text-gray-600">
-                    Temukan berbagai petualangan menakjubkan yang siap memanjakan liburan Anda
-                </p>
+  const { data: activityCategories } = useFetchActivity(endpoints.activity);
+  return (
+    <section
+      id="activity"
+      className="container px-4 py-16 mx-auto bg-gradient-to-b from-white to-primary-300"
+    >
+      {/* Judul Section */}
+      <div className="mb-12 text-center">
+        <h2 className="mb-4 text-4xl font-bold text-gray-800">
+          Explore Amazing Activities
+        </h2>
+        <p className="max-w-2xl mx-auto text-gray-600">
+          Temukan berbagai petualangan menakjubkan yang siap memanjakan liburan
+          Anda
+        </p>
+      </div>
+
+      {/* Grid Aktivitas */}
+      <div className="grid gap-5 px-5 md:grid-cols-2 lg:grid-cols-4">
+        {activityCategories?.data?.slice(0, 4).map((activity) => (
+          <div
+            key={activity.id}
+            className="overflow-hidden transition-all duration-300 bg-white shadow-lg rounded-2xl hover:shadow-xl hover:-translate-y-2 group"
+          >
+            {/* Gambar Aktivitas */}
+            <div className="relative w-full h-56">
+              <Image
+                src={activity.imageUrls[0] || "/img/favicon.ico"}
+                alt={activity.title}
+                fill
+                className="object-cover transition-transform duration-300 group-hover:scale-110"
+              />
+              {/* Ikon Aktivitas */}
+              <div className="absolute p-2 rounded-full top-4 right-4 bg-white/80">
+                <FontAwesomeIcon icon={faStar} className="text-yellow-400" />{" "}
+                <span className="text-sm font-semibold">{activity.rating}</span>
+              </div>
             </div>
 
-            {/* Grid Aktivitas */}
-            <div className="grid gap-5 px-5 md:grid-cols-2 lg:grid-cols-4">
-                {activityCategories?.data?.slice(0,4).map((activity) => (
-                    <div 
-                        key={activity.id} 
-                        className="overflow-hidden transition-all duration-300 bg-white shadow-lg rounded-2xl hover:shadow-xl hover:-translate-y-2 group"
-                    >
-                        {/* Gambar Aktivitas */}
-                        <div className="relative w-full h-56">
-                            <Image 
-                                src={activity.imageUrls[0]||"/img/favicon.ico"}
-                                alt={activity.title}
-                                fill
-                                className="object-cover transition-transform duration-300 group-hover:scale-110"
-                            />
-                            {/* Ikon Aktivitas */}
-                            <div className="absolute p-2 rounded-full top-4 right-4 bg-white/80">
-                                <FontAwesomeIcon icon={faStar} className='text-yellow-400'/> <span className='text-sm font-semibold'>{activity.rating}</span>
-                            </div>
-                        </div>
+            {/* Deskripsi Aktivitas */}
+            <div className="p-6">
+              <h3 className="mb-2 text-xl font-bold text-gray-800">
+                {activity.title}
+              </h3>
+              <p className="mb-4 text-sm text-gray-600">
+                Provincy:{activity.province}, Kota: {activity.city}
+              </p>
 
-                        {/* Deskripsi Aktivitas */}
-                        <div className="p-6">
-                            <h3 className="mb-2 text-xl font-bold text-gray-800">
-                                {activity.title}
-                            </h3>
-                            <p className="mb-4 text-sm text-gray-600">
-                               Provincy:{activity.province}, Kota: {activity.city}
-                            </p>
+              {/* Informasi Harga */}
+              <div className="flex items-center justify-between">
+                <span className="text-sm font-semibold text-red-600 line-through">
+                  Rp. {activity.price.toLocaleString()}
+                </span>
+                <span className="font-semibold text-blue-600 text-md">
+                  Rp. {activity.price_discount.toLocaleString()}
+                </span>
 
-                            {/* Informasi Harga */}
-                            <div className="flex items-center justify-between">
-                                <span className="text-sm font-semibold text-red-600 line-through">
-                                    Rp. {activity.price.toLocaleString()}
-                                </span>
-                                <span className="font-semibold text-blue-600 text-md">
-                                    Rp. {activity.price_discount.toLocaleString()}
-                                </span>
-                                
-                                <button className="px-4 py-2 text-white transition-colors bg-blue-600 rounded-lg hover:bg-blue-700">
-                                    Pesan Sekarang
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                <button className="px-4 py-2 text-white transition-colors bg-blue-600 rounded-lg hover:bg-blue-700">
+                  Pesan Sekarang
+                </button>
+              </div>
             </div>
+          </div>
+        ))}
+      </div>
 
-            {/* Tombol Lihat Semua */}
-            <div className="mt-12 text-center">
-                <Link href={'/user/activity'} className="flex items-center px-6 py-3 mx-auto space-x-2 text-gray-800 transition-colors bg-gray-100 rounded-lg w-fit hover:bg-gray-200">
-                    <FontAwesomeIcon icon={faCompass} className="w-5 h-5" />
-                    <span>Jelajahi Semua Aktivitas</span>
-                </Link>
-            </div>
-        </section>
-    );
+      {/* Tombol Lihat Semua */}
+      <div className="mt-12 text-center">
+        <Link
+          href={"/user/activity"}
+          className="flex items-center px-6 py-3 mx-auto space-x-2 text-gray-800 transition-colors bg-gray-100 rounded-lg w-fit hover:bg-gray-200"
+        >
+          <FontAwesomeIcon icon={faCompass} className="w-5 h-5" />
+          <span>Jelajahi Semua Aktivitas</span>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default ActivitySection;
--- src/components/section/PromoSection.tsx
+++ src/components/section/PromoSection.tsx
@@ -1,90 +1,125 @@
-import endpoints from '@/api/endpoints';
-import { useFetchPromo } from '@/api/hooks/Promo/useFetchPromo';
-import Image from 'next/image';
-import Link from 'next/link';
-import Slider from 'react-slick';
+import endpoints from "@/api/endpoints";
+import { useFetchPromo } from "@/api/hooks/Promo/useFetchPromo";
+import Image from "next/image";
+import Link from "next/link";
+import Slider from "react-slick";
 
 export const PromoBanner = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
-   const {data:promos, isLoading, error} = useFetchPromo(endpoints.promo)
-    const banners = [
-        {
-            "id": "cdbcb029-5ca3-44f7-83f3-852cbeeba838",
-            "name": "Disney",
-            "imageUrl": "https://travel-journal-api-bootcamp.do.dibimbing.id/images/1730792183140-disneey.jpg",
-            "createdAt": "2024-11-04T13:24:14.593Z",
-            "updatedAt": "2024-11-10T03:54:05.530Z"
-        },
-        {
-            "id": "9b93043d-34fb-481c-bd18-5a4dec44df3c",
-            "name": "Australia",
-            "imageUrl": "https://travel-journal-api-bootcamp.do.dibimbing.id/images/1731498367395-191011110307-02-australia-beautiful-places.jpg",
-            "createdAt": "2024-11-13T11:46:07.532Z",
-            "updatedAt": "2024-11-13T11:46:07.532Z"
-        },
-        {
-            "id": "803ad66c-1641-49bf-a5bd-970140bc1683",
-            "name": "Banner edit",
-            "imageUrl": "https://www.ancol.com/shared/file-manager/Unit%20Informasi/DUFAN/WAHANA-DUFAN---ICE-AGE.jpg",
-            "createdAt": "2024-11-10T03:08:20.874Z",
-            "updatedAt": "2024-11-13T15:26:20.279Z"
-        }
-    ]
-    return(
-        <section className="container py-5 mx-auto bg-gradient-to-b from-gray-100 to-white"  id='promo'>
-        {/* Banner */}
-        <h1 className='mb-2 text-2xl text-center md:text-6xl text-primary-300 font-tittle'>Promo Terbaru</h1>
-        <div className="container px-10 mx-auto">
-                <Slider {...settings}>
-                        {banners.map((banner, index) => (
-                            <div key={index} className="relative flex items-center justify-center h-64 gap-10">
-                                <Image
-                                    width={1000}
-                                    height={1000}
-                                    src={banner.imageUrl}
-                                    alt={banner.name}
-                                    className="object-cover w-full h-full rounded-lg shadow-md"
-                                />
-                                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-lg">
-                                    <h1 className="text-3xl font-bold text-center text-white">{banner.name}</h1>
-                                </div>
-                            </div>
-                        ))}
-                    </Slider>
-        </div>
-        {/* Promo List */}
-        <div className="flex flex-col w-full p-4 mt-5">
-            {/* <h2 className="text-2xl font-semibold">Promo Terbaru</h2> */}
-            <Link href={'/'} className='self-end mr-5 hover:underline hover:text-primary-200'>See All</Link>
-            <ul className="grid grid-cols-1 gap-3 px-6 mt-4 md:grid-cols-3">
-                {isLoading ? <p>Loading...</p> : promos?.data?.slice(0,3).map((promo) => (
-                    <li key={promo.id} className="overflow-auto transition rounded-lg shadow hover:shadow-primary-300 hover:shadow-lg">
-                        <Image
-                            width={1000}
-                            height={100}
-                            alt={promo.title}
-                            src={promo.imageUrl}
-                            className="object-cover object-center w-full h-36"
-                        />
-                        <div className="flex flex-col w-full px-4 py-2 bg-white">
-                            <a href="#">
-                                <h3 className="mt-0.5 text-lg text-gray-900">{promo.title}</h3>
-                            </a>
-                            <p className="mt-2 text-gray-500 line-clamp-3 text-sm/relaxed">
-                               Diskon <span className='font-bold'>Rp. {promo.promo_discount_price.toLocaleString()}</span>
-                            </p>
-                            <Link href={`/user/promo/${promo.id}`} className='inline-flex items-center self-end gap-2 px-8 py-3 text-white border rounded border-primary-300 bg-primary-300 hover:bg-transparent hover:text-primary-300 focus:outline-none focus:ring active:text-primary-100'>Lihat Promo</Link>
-                        </div>
-                    </li>
-                )) }
-            </ul>
-        </div>
-        </section>
-    )
-}
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  const { data: promos, isLoading } = useFetchPromo(endpoints.promo);
+  const banners = [
+    {
+      id: "cdbcb029-5ca3-44f7-83f3-852cbeeba838",
+      name: "Disney",
+      imageUrl:
+        "https://travel-journal-api-bootcamp.do.dibimbing.id/images/1730792183140-disneey.jpg",
+      createdAt: "2024-11-04T13:24:14.593Z",
+      updatedAt: "2024-11-10T03:54:05.530Z",
+    },
+    {
+      id: "9b93043d-34fb-481c-bd18-5a4dec44df3c",
+      name: "Australia",
+      imageUrl:
+        "https://travel-journal-api-bootcamp.do.dibimbing.id/images/1731498367395-191011110307-02-australia-beautiful-places.jpg",
+      createdAt: "2024-11-13T11:46:07.532Z",
+      updatedAt: "2024-11-13T11:46:07.532Z",
+    },
+    {
+      id: "803ad66c-1641-49bf-a5bd-970140bc1683",
+      name: "Banner edit",
+      imageUrl:
+        "https://www.ancol.com/shared/file-manager/Unit%20Informasi/DUFAN/WAHANA-DUFAN---ICE-AGE.jpg",
+      createdAt: "2024-11-10T03:08:20.874Z",
+      updatedAt: "2024-11-13T15:26:20.279Z",
+    },
+  ];
+  return (
+    <section
+      className="container py-5 mx-auto bg-gradient-to-b from-gray-100 to-white"
+      id="promo"
+    >
+      {/* Banner */}
+      <h1 className="mb-2 text-2xl text-center md:text-6xl text-primary-300 font-tittle">
+        Promo Terbaru
+      </h1>
+      <div className="container px-10 mx-auto">
+        <Slider {...settings}>
+          {banners.map((banner, index) => (
+            <div
+              key={index}
+              className="relative flex items-center justify-center h-64 gap-10"
+            >
+              <Image
+                width={1000}
+                height={1000}
+                src={banner.imageUrl}
+                alt={banner.name}
+                className="object-cover w-full h-full rounded-lg shadow-md"
+              />
+              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-lg">
+                <h1 className="text-3xl font-bold text-center text-white">
+                  {banner.name}
+                </h1>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+      {/* Promo List */}
+      <div className="flex flex-col w-full p-4 mt-5">
+        {/* <h2 className="text-2xl font-semibold">Promo Terbaru</h2> */}
+        <Link
+          href={"/"}
+          className="self-end mr-5 hover:underline hover:text-primary-200"
+        >
+          See All
+        </Link>
+        <ul className="grid grid-cols-1 gap-3 px-6 mt-4 md:grid-cols-3">
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            promos?.data?.slice(0, 3).map((promo) => (
+              <li
+                key={promo.id}
+                className="overflow-auto transition rounded-lg shadow hover:shadow-primary-300 hover:shadow-lg"
+              >
+                <Image
+                  width={1000}
+                  height={100}
+                  alt={promo.title}
+                  src={promo.imageUrl}
+                  className="object-cover object-center w-full h-36"
+                />
+                <div className="flex flex-col w-full px-4 py-2 bg-white">
+                  <a href="#">
+                    <h3 className="mt-0.5 text-lg text-gray-900">
+                      {promo.title}
+                    </h3>
+                  </a>
+                  <p className="mt-2 text-gray-500 line-clamp-3 text-sm/relaxed">
+                    Diskon{" "}
+                    <span className="font-bold">
+                      Rp. {promo.promo_discount_price.toLocaleString()}
+                    </span>
+                  </p>
+                  <Link
+                    href={`/user/promo/${promo.id}`}
+                    className="inline-flex items-center self-end gap-2 px-8 py-3 text-white border rounded border-primary-300 bg-primary-300 hover:bg-transparent hover:text-primary-300 focus:outline-none focus:ring active:text-primary-100"
+                  >
+                    Lihat Promo
+                  </Link>
+                </div>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+    </section>
+  );
+};
